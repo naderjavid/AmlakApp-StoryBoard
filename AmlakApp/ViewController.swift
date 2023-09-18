194f7c1fdbb09b76f4856ba3b19c19a663c19061
Nader Javid <nader.javid@gmail.com>
2023-09-18 22:26:05 +0330

    Initial Commit

---
+++ AmlakApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmlakApp
+//
+//  Created by nader on 6/27/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
